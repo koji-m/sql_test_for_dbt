9c66c27bcc1e7a1f6832c775597453bf36eacda2
Koji Matsumoto <otomustam.ijok@gmail.com>
2023-12-10 14:10:20 +0900

    lint

--- duck_test/tests/test_models.py
+++ duck_test/tests/test_models.py
@@ -3,15 +3,18 @@ import subprocess
 import duckdb
 import pytest
 
+
 @pytest.fixture(scope="session")
 def dbt_compile():
     subprocess.run(["poetry", "run", "dbt", "compile", "--target", "test"], check=True)
+
 
 @pytest.fixture
 def duck_con(dbt_compile):
     con = duckdb.connect("test.duckdb")
     yield con
     con.close()
+
 
 @pytest.fixture
 def distro_using(duck_con):
@@ -20,11 +23,12 @@ def distro_using(duck_con):
     yield duck_con
     duck_con.sql("DROP TABLE linux_distro")
 
+
 def test_distro_using(distro_using):
-    with open('./target/compiled/duck_test/models/distro_using.sql') as f:
+    with open("./target/compiled/duck_test/models/distro_using.sql") as f:
         sql = f.read()
 
-    expected = [(1, 'Ubuntu')]
+    expected = [(1, "Ubuntu")]
     result = distro_using.sql(sql).fetchall()
 
     assert result == expected
